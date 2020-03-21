8e0d0862a559c0ff0849eb023bd7d726e1b47357
Arpita <gaonkar.a@husky.neu.edu>
2020-03-21 00:39:30 -0400

    1st change

--- demo/src/main/java/com/example/demo/GitJenkins1Application.java
+++ demo/src/main/java/com/example/demo/GitJenkins1Application.java
@@ -8,6 +8,8 @@ public class GitJenkins1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitJenkins1Application.class, args);
+		
+		System.out.println("1st jenkins project");
 	}
 
 }
